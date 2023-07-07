b38b93b4f1c8b4388aaa9cd139fae8dd2efdfd01
NKUmianman <1277463846@qq.com>
2023-07-07 22:20:33 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
     runtimeCompiler: true,
+    devServer: {
+        hot: true,
+    },
 });
